371eda6b1980501899c897f4407c55056ac2121f
Chen, Hung <Hung.Chen@Pearson.com>
2016-11-23 09:08:48 -0500

    added file protocol

--- test/test.pluckygit.js
+++ test/test.pluckygit.js
@@ -40,7 +40,7 @@ describe('PluckyGit', ()=>{
 	it('should return 0 and a result string when cloning to a new directory', (done) => {
 		const git = new PluckyGit();	
 		const repo = path.join(process.cwd(), 'testdirectory/.git');
-		git.handler({params: {repository: repo, folder:'clonedtestdirectory'}}, (code, val) => {
+		git.handler({params: {repository: `file://${repo}`, folder:'clonedtestdirectory'}}, (code, val) => {
 			expect(code).to.equal(0);
 			expect(val.result).to.be.a.string();
 			fs.remove('clonedtestdirectory', (err) => {
@@ -61,10 +61,10 @@ describe('PluckyGit', ()=>{
 	it('should clone first and then do fastforward merge', (done) => {
 		const git = new PluckyGit();
 		const repo = path.join(process.cwd(), 'testdirectory/.git');
-		git.handler({params: {repository: repo, folder:'clonedtestdirectory'}}, (code, val) => {
+		git.handler({params: {repository: `file://${repo}`, folder:'clonedtestdirectory'}}, (code, val) => {
 			expect(code).to.equal(0);
 			expect(val.result).to.be.a.string();
-			git.handler({params: {repository: repo, folder:'clonedtestdirectory'}}, (code, val) => {
+			git.handler({params: {repository: `file://${repo}`, folder:'clonedtestdirectory'}}, (code, val) => {
 				expect(code).to.equal(0);
 				expect(val.result).to.be.a.string();
 				fs.remove('clonedtestdirectory', (err) => {
